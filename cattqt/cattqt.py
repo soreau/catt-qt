78e1aa3756b992f7d3b2d65178bad4a1c7821c44
Dashon Wells <sparky123558wify@gmail.com>
2022-06-13 22:02:29 -0500

    Revert "Hopefully this fixes those unintentional changes"
    This reverts commit fa81adf5b64f1a93bd0b687427efe4eb6d07e580.
    
    Undo screw up 3

--- cattqt/cattqt.py
+++ cattqt/cattqt.py
@@ -676,6 +676,7 @@ class App(QMainWindow):
         self.textbox.setText(text)
 
     def on_start_singleshot_timer(self, d):
+        d.playback_starting = False
         d.playback_just_started = True
         d.just_started_timer.start(2000)
 
@@ -697,7 +698,6 @@ class App(QMainWindow):
             print('Failed to play "%s" please include full path' % text)
             return
         self.on_stop_signal(d)
-        d.stopping_timer.stop()
         d.kill_catt_process()
         self.status_label.setText("Playing..")
         try:
@@ -1124,7 +1124,7 @@ class MediaListener:
         if (
             d.filename != None
             and status.idle_reason == "FINISHED"
-            and status.title == d.filename
+            and status.title == os.path.splitext(d.filename)[0]
         ):
             d.kill_catt_process()
             s.stop_call.emit(d)
@@ -1214,7 +1214,7 @@ class ConnectionListener:
 
 author = "Scott Moreau"
 email = "oreaus@gmail.com"
-version = "2.9"
+version = "3.0"
 
 
 def main() -> None:
